56684cbf385fc7f47bf9188ec44631b394a4b982
Patrick Koidal <pkoidal519@gmail.com>
2017-09-07 22:02:32 -0500

    maybe cursor and db call
    ??? :)

--- logs.py
+++ logs.py
@@ -1,1 +1,10 @@
 import pyscopg2
+
+def main ():
+    conn_string = "host='localhost' dbname='news'"
+    conn = psycopg2.connection(conn_string)
+    cursor = conn.cursor()
+    print "Connected\n"
+
+
+    

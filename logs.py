18ea3365694cef449469daf4227eff98ef44bb9f
Patrick Koidal <pkoidal519@gmail.com>
2017-09-11 19:44:56 -0500

    update 3 i think
    update cursor and add sql boilerplate

--- logs.py
+++ logs.py
@@ -1,10 +1,26 @@
 import pyscopg2
+DB_NAME = "news"
 
-def main ():
-    conn_string = "host='localhost' dbname='news'"
-    conn = psycopg2.connection(conn_string)
-    cursor = conn.cursor()
-    print "Connected\n"
+query1 = "select..."
 
+query2 = "select..."
 
-    
+query3 = "select..."
+
+#store results
+query1_returns =
+query1_returns['title'] =
+
+query2_returns =
+query2_returns['title'] =
+
+query3_returns = dict()
+query3_returns['title'] =
+
+def return_query(query)
+    db = psycopg2.connect(database=DB_NAME)
+    c = db.cursor()
+    c.execute(query)
+    results = c.fetchall()
+    db.close()
+    return results
